31814fa6c5b79eaf9434830ef04863a8e72f508a
TopGun1405 <fstopgun0317@gmail.com>
2024-05-27 12:17:40 +0900

    fix_dir_name

--- quazar/apps/models.py
+++ quazar/apps/models.py
@@ -7,9 +7,21 @@ class Weather(models.Model):
     date_time = models.DateTimeField()
 
     temperature = models.FloatField()
+    feels_like = models.FloatField()
+
     humidity = models.FloatField()
     wind_speed = models.FloatField()
     wind_direction = models.CharField()
     precipitation = models.IntegerField()
     pressure = models.IntegerField()
     weather_condition = models.CharField()
+    visibility = models.IntegerField()
+    uv_index = models.CharField()
+
+    sunrise = models.TimeField()
+    sunset = models.TimeField()
+    moonrise = models.TimeField()
+    moonset = models.TimeField()
+    moon_phase = models.CharField()
+
+    dew_point = models.FloatField()
